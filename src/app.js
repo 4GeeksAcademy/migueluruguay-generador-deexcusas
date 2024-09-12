e1023c854c09bd4ca1a4f9bdad65fd911933a066
migueluruguay <miguelangelsilveirafagundez@gmail.com>
2024-09-12 02:16:51 +0000

    generador-de-excusas

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
@@ -8,4 +18,16 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let excusa = document.getElementById("excusa");
+  function generarexcusa() {
+    excusa.innerHTML =
+      who[Math.floor(Math.random() * 4)] +
+      " " +
+      action[Math.floor(Math.random() * 4)] +
+      " " +
+      what[Math.floor(Math.random() * 3)] +
+      " " +
+      when[Math.floor(Math.random() * 5)];
+  }
+  generarexcusa();
 };
